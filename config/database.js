bafdfabeb54f15a8118d756e7079fc5aa88f739b
aleksandar <aca.djordjevic1113@gmail.com>
2020-11-02 00:45:22 -0500

    a

--- config/database.js
+++ config/database.js
@@ -11,7 +11,7 @@ module.exports = {
     username,
     password,
     database,
-    host,
+    // host,
     dialect: 'postgres',
   },
   production: {
--- routes/api/homes.js
+++ routes/api/homes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { Homes } = require("../../db/models");
+const { Homes, Reviews } = require("../../db/models");
 
 const router = express.Router();
 const { asyncHandler} = require("../utils");
@@ -19,6 +19,17 @@ router.get('/', asyncHandler(async (req, res) => {
 router.get('/:id', asyncHandler(async function (req,res) {
   const home = await Homes.findByPk(req.params.id);
   res.json(home);
+}))
+
+router.get('/:id/reviews', asyncHandler(async function (req, res) {
+    const reviews = await Reviews.findAll({
+      where: {
+        homeId: req.params.id
+      },
+      order: [['createdAt', 'DESC']],
+    })
+
+    res.json({reviews})
 }))
 
 module.exports = router
